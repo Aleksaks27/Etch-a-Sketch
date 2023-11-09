381eba196fac7d5b85c79420e7024ddac6b83de8
aleksaks27 <aleksaks27@gmail.com>
2023-11-09 14:19:21 +0000

    Enable users to choose canvas size and to erase their drawings

--- app.js
+++ app.js
@@ -5,21 +5,53 @@
 // different colours and perhaps erase the board entirely.
 
 const canvas = document.querySelector("#canvas");
-let size = 16;
-// const changeColor = () => {
-//     this.setAttribute("class", "change")
-// }
+const clear = document.querySelector("#clear");
+const input = document.querySelector("#size");
 
-for (let i = 1; i <= size; i++) {
-    let line = document.createElement("div");
-    for (let j = 1; j <= size; j++) {
-        let pixel = document.createElement("div");
-        pixel.classList.add("pixel")
-        pixel.addEventListener("click", () => {
-            pixel.classList.add("change");
-        })
-        line.append(pixel);
+function createGrid(size, pixelSize) {
+    for (let i = 1; i <= size; i++) {
+        let line = document.createElement("div");
+        line.style.height = `${pixelSize}px`
+        for (let j = 1; j <= size; j++) {
+            let pixel = document.createElement("div");
+            pixel.style.cssText = `width: ${pixelSize}px; height: ${pixelSize}px`
+            pixel.classList.add("pixel");
+            line.append(pixel);
+        }
+        line.classList.add("line");
+        canvas.append(line);
     }
-    line.classList.add("line")
-    canvas.append(line);
 }
+
+function draw() {
+    const pixels = document.querySelectorAll(".pixel");
+    pixels.forEach(pixel => pixel.addEventListener("click", () => {
+        pixel.classList.add("draw");
+    }))
+}
+
+function erase() {
+    const pixels = document.querySelectorAll(".pixel");
+    const lines = document.querySelectorAll(".line");
+    pixels.forEach(pixel => pixel.remove());
+    lines.forEach(line => line.remove())
+}
+
+function initialize() {
+    let size = input.value;
+    let pixelSize = (400/size);
+    createGrid(size, pixelSize);
+    draw();
+}
+
+input.addEventListener("change", () => {
+    erase();
+    initialize()
+});
+
+clear.addEventListener("click", () => {
+    erase();
+    initialize();
+});
+
+initialize();
